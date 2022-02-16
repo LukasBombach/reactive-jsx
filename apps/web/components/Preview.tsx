0094922c9bfb21c1127316ab60183a3d87743e0e
Lukas Bombach <lukas.bombach@googlemail.com>
2022-02-16 16:47:09 +0100

    tabularasa on the preview code

--- apps/web/components/Preview.tsx
+++ apps/web/components/Preview.tsx
@@ -1,70 +1,20 @@
 import { useState /* useEffect */ } from "react";
 import { createPortal } from "react-dom";
 
-import type { VFC } from "react";
+import type { VFC, FC } from "react";
 
 type PreviewProps = Omit<JSX.IntrinsicElements["iframe"], "children"> & { code: string };
 
-// const createScript = (code: string) => {
-//   const script = document.createElement("script");
-//   script.textContent = code;
-//   return script;
-// };
-
 export const Preview: VFC<PreviewProps> = ({ code, ...props }) => {
-  // const [script, setScript] = useState<HTMLScriptElement | null>(null);
-  // const [iframe, setIframe] = useState<HTMLIFrameElement | null>(null);
-
-  // console.log("render", { iframe, code });
-
-  // const body = iframe?.contentWindow?.document?.body;
-
-  // useEffect(() => {
-  // if (!body) return;
-
-  // console.log("useEffect", { iframe, code });
-
-  // body.innerHTML = `<pre>${code}</pre>`;
-
-  /* const safeCode = `
-      try {
-        ${code};
-      } catch(error) {
-        document.body.innerHTML = \`<pre>\${error.message}</pre>\`;
-      }
-    `;
-
-    if (!script) {
-      const newScript = createScript(safeCode);
-      head.append(newScript);
-      setScript(newScript);
-    } else if (script.textContent !== safeCode) {
-      const newScript = createScript(safeCode);
-      script.parentNode.removeChild(script);
-      body.replaceChildren();
-      head.append(newScript);
-      setScript(newScript);
-    } */
-  // }, [iframe, code]);
-
   return (
-    <Iframe>
+    <Iframe {...props}>
       <pre>${code}</pre>
     </Iframe>
   );
-
-  /* return (
-    <iframe {...props} ref={setIframe}>
-      {body && createPortal(() => <pre>${code}</pre>, body)}
-    </iframe>
-  ); */
-
-  // return <iframe {...props} ref={setIframe} />;
 };
 
-const Iframe = ({ children, ...props }) => {
+const Iframe: FC<JSX.IntrinsicElements["iframe"]> = ({ children, ...props }) => {
   const [ref, setRef] = useState(null);
-
   const body = ref?.contentWindow.document.body;
 
   return (
