5084a60096ce265c2adbd4b7153dde2d5b7dac27
Lukas Bombach <lukas.bombach@googlemail.com>
2022-02-22 20:03:46 +0100

    getting simewhere

--- apps/playground/pages/test.tsx
+++ apps/playground/pages/test.tsx
@@ -4,39 +4,52 @@ import type { NextPage } from "next";
 type Read<T> = () => T;
 type Write<T> = (value: T) => void;
 
-type Value<T> = Read<T> & Write<T>;
-type Reaction<T, R extends T | void> = (fn: (value: T) => R) => Read<R>;
+// type Value<T> = Read<T> & Write<T>;
+// type Reaction<T, R extends T | void> = (fn: (value: T) => R) => Read<R>;
+// const currentReaction = new Set<Value<unknown>>();
+// type Subscribers = { subscribers: Set<unknown> };
 
+type Reaction<T> = (value: T) => T;
+
+let currentReacton: Reaction<any> | null = null;
+
+// todo cleanup
 function value<T>(initial: T): Read<T> & Write<T> {
   let current = initial;
+  const subscribers = new Set<Reaction<any>>();
   return (next?: T) => {
     if (next) {
       current = next;
+      subscribers.forEach(fn => fn(current));
     } else {
+      if (currentReacton) {
+        subscribers.add(currentReacton);
+      }
       return current;
     }
   };
 }
 
-function reaction<T, R = T | void>(fn: (value: R) => R): Read<R> {
+function reaction<T, R = T | void>(fn: Reaction<R>): Read<R> {
+  currentReacton = fn;
   let current = fn(null);
+  currentReacton = null;
   return () => {
+    currentReacton = fn;
     current = fn(current);
+    currentReacton = null;
     return current;
   };
 }
 
 const num = value(2);
+const double = reaction(() => num() * 2);
+
 reaction(() => console.log("num", num()));
+reaction(() => console.log("double", double()));
+
 num(4);
-
-// const double = reaction(() => num() * 2);
-// reaction(() => console.log("double", double()));
-// num(4);
-
-// setInterval(() => {
-//   num(num() + 1);
-// }, 1000);
+num(6);
 
 const Home: NextPage = () => {
   return <main className="h-screen w-full bg-[#282c34]">hello</main>;
