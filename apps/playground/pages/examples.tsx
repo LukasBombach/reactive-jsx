eb82a7687bd9810b3d5388e734186c9071abf7c8
Lukas Bombach <lukas.bombach@googlemail.com>
2022-03-11 22:44:38 +0100

    components!

--- apps/playground/pages/examples.tsx
+++ apps/playground/pages/examples.tsx
@@ -9,7 +9,7 @@ const Example = dynamic(() => import("components/Example"), { ssr: false });
 const helloWorld = `
 const name = 'world';
 
-const el = <h1>Hello {name}!</h1>
+const el = <h1>Hello {name}</h1>;
 
 const root = document.getElementById("app");
 root.append(el);`;
@@ -18,7 +18,45 @@ const dynamicAttributes = `
 const src = '/tutorial/image.gif';
 const name = 'Rick Astley';
 
-const el = <img src={src} alt={\`$\{name} dancing\`} />
+const el = <img src={src} alt={\`$\{name} dancing\`} />;
+
+const root = document.getElementById("app");
+root.append(el);`;
+
+const components = `
+const Component = () => {
+  const name = 'world';
+
+  return <h1>Hello {name}</h1>;
+};
+
+const root = document.getElementById("app");
+root.append(<Component />);`;
+
+const nestedComponents = `
+const Nested = () => {
+  return <em>nests this component</em>;
+};
+
+const Component = () => {
+  return <h1>This component <Nested /></h1>;
+};
+
+const root = document.getElementById("app");
+root.append(<Component />);`;
+
+const reactiveAssignments = `
+let count = 0;
+
+function handleClick() {
+  count += 1;
+}
+
+const el = (
+  <button onClick={handleClick}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+  </button>
+)
 
 const root = document.getElementById("app");
 root.append(el);`;
@@ -30,6 +68,12 @@ const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = (
       <Example source={helloWorld} runtime={runtime} />
       Dynamic Attributes
       <Example source={dynamicAttributes} runtime={runtime} className="h-[280px]" />
+      Components
+      <Example source={components} runtime={runtime} />
+      Nested Components
+      <Example source={nestedComponents} runtime={runtime} />
+      Reactive Assignments
+      <Example source={reactiveAssignments} runtime={runtime} />
     </main>
   );
 };
