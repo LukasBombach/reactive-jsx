305562b9fca385b7f8fe29ff5962be6c99b5d8c0
Lukas Bombach <lukas.bombach@googlemail.com>
2022-04-12 16:13:25 +0200

    smal cleanup

--- packages/playground/compiler.ts
+++ packages/playground/compiler.ts
@@ -14,6 +14,7 @@ export async function compile(source: string, resolveFile: ResolveFile): Promise
     input: sourceFileName,
     plugins: [fs(source, resolveFile), babel()],
   });
+
   const { output } = await bundle.generate(outputOptions);
   await bundle.close();
   return output[0].code;
