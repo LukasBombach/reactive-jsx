57c9425f1bdadd9dd7839a2531b8818b9aa8af04
Lukas Bombach <lukas.bombach@googlemail.com>
2022-04-13 12:41:11 +0200

    upgraded all the things

--- packages/playground/useCompiler.ts
+++ packages/playground/useCompiler.ts
@@ -1,13 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useId } from "react";
 import { compile } from "./compiler";
 
 import type { ResolveFile } from "./compiler";
 
-const worker = new Worker(new URL("./compiler.worker.ts", import.meta.url));
+let worker: Worker | undefined;
 
-worker.onmessage = function (e) {
-  console.log("Message received from worker", e.data);
-};
+if (typeof Worker !== "undefined") {
+  worker = new Worker(new URL("./compiler.worker.ts", import.meta.url));
+
+  worker.onmessage = function (e) {
+    console.log("Message received from worker", e.data);
+  };
+}
 
 export function useCompiler(
   initialSource: string,
@@ -15,10 +19,13 @@ export function useCompiler(
 ): [compiledSource: string | undefined, setSource: (source: string) => void] {
   const [source, setSource] = useState(initialSource);
   const [compiledSource, setCompiledSource] = useState<string>();
+  const id = useId();
 
   // todo race conditions
   useEffect(() => {
-    worker.postMessage(source);
+    if (worker) {
+      worker.postMessage(source);
+    }
 
     compile(source, resolveFile)
       .then(src => setCompiledSource(src.trim()))
@@ -27,3 +34,5 @@ export function useCompiler(
 
   return [compiledSource, setSource];
 }
+
+function useWorker() {}
