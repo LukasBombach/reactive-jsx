fb339aae71ff90f4dfc18278b2d32af989cdf7a3
Lukas Bombach <lukas.bombach@googlemail.com>
2022-04-18 13:09:05 +0200

    pane without gap

--- packages/playground/Playground.tsx
+++ packages/playground/Playground.tsx
@@ -11,30 +11,28 @@ export const Playground: VFC<{ source: string; resolveFile: ResolveFile }> = ({ 
   const [compiledSource, setSource] = useCompiler(source, resolveFile);
 
   return (
-    <section className="bg-[#282c34] rounded-xl overflow-hidden drop-shadow-md">
-      <header className="grid grid-cols-12 gap-4 p-6 bg-[#ffffff0a] px-6 py-3">
-        <div className="col-span-7 flex gap-4">
-          <span className="text-xs text-slate-200 py-1">app.js</span>
+    <SplitPane>
+      <div>
+        <div className="grid grid-cols-12 gap-4 p-6 bg-[#ffffff0a] px-6 py-3">
+          <div className="col-span-7 flex gap-4">
+            <span className="text-xs text-slate-200 py-1">app.js</span>
+          </div>
         </div>
-        <div className="col-span-5 flex gap-4">
-          <button className="text-xs text-slate-200 py-1 cursor-pointer">Result</button>
-          <button className="text-xs text-slate-200 py-1 border-b border-amber-400 cursor-pointer">JS Output</button>
-        </div>
-      </header>
-      <SplitPane>
         <Editor className="p-6" value={source} onChange={setSource} />
+      </div>
+      <div>
+        <div className="grid grid-cols-12 gap-4 p-6 bg-[#ffffff0a] px-6 py-3">
+          <div className="col-span-5 flex gap-4">
+            <button className="text-xs text-slate-200 py-1 cursor-pointer">Result</button>
+            <button className="text-xs text-slate-200 py-1 border-b border-amber-400 cursor-pointer">JS Output</button>
+          </div>
+        </div>
         <Editor className="p-6" value={compiledSource} readOnly />
-      </SplitPane>
-    </section>
+      </div>
+    </SplitPane>
   );
 };
 
 const Editor: VFC<ReactCodeMirrorProps> = props => (
-  <CodeMirror
-    basicSetup={false}
-    theme="dark"
-    height="100%"
-    extensions={[javascript({ jsx: true, typescript: true })]}
-    {...props}
-  />
+  <CodeMirror basicSetup={false} theme="dark" height="100%" extensions={[javascript({ jsx: true })]} {...props} />
 );
--- packages/playground/SplitPane.tsx
+++ packages/playground/SplitPane.tsx
@@ -7,6 +7,7 @@ import type { FC, MouseEventHandler, ReactNode } from "react";
 // todo respect hor. scrolling in calcs
 export const SplitPane: FC<{ children: [ReactNode, ReactNode]; intitialPos?: number }> = ({ children }) => {
   const container = useRef<HTMLDivElement | null>(null);
+  const handle = useRef<HTMLDivElement | null>(null);
   const mousePos = useRef<number | null>(null);
   const [left, right] = children;
 
@@ -18,12 +19,13 @@ export const SplitPane: FC<{ children: [ReactNode, ReactNode]; intitialPos?: num
   };
 
   const onMouseMove = (e: MouseEvent) => {
-    if (!mousePos.current || !container.current) return;
+    if (!mousePos.current || !container.current || !handle.current) return;
     const c = container.current.getBoundingClientRect();
     const left = e.clientX - mousePos.current - c.left - 24; // todo scrolling
     const pos = (left / (c.width - 48)) * 100;
     const cappedPos = Math.min(Math.max(pos, 10), 90);
     container.current.style.setProperty("--split", cappedPos + "%");
+    handle.current?.style.setProperty("--split", cappedPos + "%");
   };
 
   const onMouseUp = () => (mousePos.current = null);
@@ -38,12 +40,17 @@ export const SplitPane: FC<{ children: [ReactNode, ReactNode]; intitialPos?: num
   });
 
   return (
-    <div className="grid grid-rows-1 grid-cols-[var(--split,50%)_14px_1fr]" ref={container}>
+    <section
+      className="relative grid grid-rows-1 grid-cols-[var(--split,50%)_1fr] bg-[#282c34] rounded-xl overflow-hidden drop-shadow-md"
+      ref={container}
+    >
       {left}
-      <div className="grid place-content-center">
-        <div className="w-1 h-24 bg-[#ffffff05] rounded-xl cursor-col-resize" onMouseDown={onMouseDown} />
-      </div>
       {right}
-    </div>
+      <div
+        className="absolute w-1 h-24 bg-[#ffffff10] rounded-xl cursor-col-resize top-[50%] left-[var(--split,50%)] -translate-y-[50%]"
+        onMouseDown={onMouseDown}
+        ref={handle}
+      />
+    </section>
   );
 };
