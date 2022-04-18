592f5636772adaab648ca9cd22dc1631a64c3f34
Lukas Bombach <lukas.bombach@googlemail.com>
2022-04-18 12:50:39 +0200

    got something that can be worked with

--- packages/playground/SplitPane.tsx
+++ packages/playground/SplitPane.tsx
@@ -1,31 +1,32 @@
-import { useState, useEffect, useRef } from "react";
-import { splitPane } from "./playground.module.css";
+import { useEffect, useRef } from "react";
 
 import type { FC, MouseEventHandler, ReactNode } from "react";
 
-export const SplitPane: FC<{ children: [ReactNode, ReactNode]; intitialPos?: number }> = ({
-  children,
-  intitialPos = 55,
-}) => {
+// todo cleanup
+// todo moving the pane has a little overshoot left and right
+// todo respect hor. scrolling in calcs
+export const SplitPane: FC<{ children: [ReactNode, ReactNode]; intitialPos?: number }> = ({ children }) => {
   const container = useRef<HTMLDivElement | null>(null);
   const mousePos = useRef<number | null>(null);
-  const [pos, setPos] = useState(intitialPos);
   const [left, right] = children;
 
   const onMouseDown: MouseEventHandler<HTMLDivElement> = e => {
-    var rect = e.target.getBoundingClientRect();
+    e.preventDefault();
+    e.stopPropagation();
+    var rect = (e.target as HTMLDivElement).getBoundingClientRect();
     mousePos.current = e.clientX - rect.left;
   };
-
-  const onMouseUp = () => (mousePos.current = null);
 
   const onMouseMove = (e: MouseEvent) => {
     if (!mousePos.current || !container.current) return;
     const c = container.current.getBoundingClientRect();
     const left = e.clientX - mousePos.current - c.left - 24; // todo scrolling
-    const newPos = (left / (c.width - 48)) * 100;
-    setPos(newPos);
+    const pos = (left / (c.width - 48)) * 100;
+    const cappedPos = Math.min(Math.max(pos, 10), 90);
+    container.current.style.setProperty("--split", cappedPos + "%");
   };
+
+  const onMouseUp = () => (mousePos.current = null);
 
   useEffect(() => {
     document.addEventListener("mousemove", onMouseMove);
@@ -37,7 +38,7 @@ export const SplitPane: FC<{ children: [ReactNode, ReactNode]; intitialPos?: num
   });
 
   return (
-    <div className={["p-6", splitPane].join(" ")} style={{ "--split-at": `${pos}%` }} ref={container}>
+    <div className="grid grid-rows-1 grid-cols-[var(--split,50%)_14px_1fr]" ref={container}>
       {left}
       <div className="grid place-content-center">
         <div className="w-1 h-24 bg-[#ffffff05] rounded-xl cursor-col-resize" onMouseDown={onMouseDown} />
