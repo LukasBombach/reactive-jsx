6e0de1bf15b286b3c322ff9a0aba5510ba166ddb
Lukas Bombach <lukas.bombach@googlemail.com>
2022-04-19 14:09:25 +0200

    got export as string

--- packages/runtime/scripts/exportAsString.js
+++ packages/runtime/scripts/exportAsString.js
@@ -2,10 +2,10 @@ import { promises as fs } from "fs";
 import { resolve } from "path";
 
 async function exportAsString() {
-  const pathToSource = resolve(import.meta.url, "../dist", "index.modern.js");
-  const pathToGeneratedFile = resolve(import.meta.url, "../dist", "index.modern.asStr.js");
+  const pathToSource = resolve("dist", "index.modern.js");
+  const pathToGeneratedFile = resolve("dist", "index.modern.asStr.js");
   const sourceCode = await fs.readFile(pathToSource, "utf-8");
-  const generatedCode = `module.exports = ${JSON.stringify(sourceCode)}`;
+  const generatedCode = `module.exports = ${JSON.stringify(sourceCode)};`;
   await fs.writeFile(pathToGeneratedFile, generatedCode, "utf-8");
 }
 
