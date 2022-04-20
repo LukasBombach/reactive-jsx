bfaa94901e06af0ba466cf3b6073777d0d3b5a30
Lukas Bombach <lukas.bombach@googlemail.com>
2022-04-20 10:11:35 +0200

    implemented the tabs in the most shitty way

--- packages/playground/Playground.tsx
+++ packages/playground/Playground.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { useCompiler } from "./useCompiler";
@@ -11,6 +12,7 @@ import type { ResolveFile } from "./compiler";
 // todo optimize markup & css
 export const Playground: VFC<{ source: string; resolveFile: ResolveFile }> = ({ source, resolveFile }) => {
   const [compiledSource, setSource] = useCompiler(source, resolveFile);
+  const [rightPane, setRightPane] = useState<"result" | "js">("result");
 
   return (
     <SplitPane>
@@ -25,12 +27,31 @@ export const Playground: VFC<{ source: string; resolveFile: ResolveFile }> = ({ 
       <div>
         <div className="grid grid-cols-12 gap-4 p-6 bg-[#ffffff0a] px-6 py-3">
           <div className="col-span-5 flex gap-4">
-            <button className="text-xs text-slate-200 py-1 cursor-pointer">Result</button>
-            <button className="text-xs text-slate-200 py-1 border-b border-amber-400 cursor-pointer">JS Output</button>
+            <button
+              className={[
+                "text-xs text-slate-200 py-1 cursor-pointer",
+                rightPane === "result" && "border-b border-amber-400",
+              ].join(" ")}
+              onClick={() => setRightPane("result")}
+            >
+              Result
+            </button>
+            <button
+              className={[
+                "text-xs text-slate-200 py-1 cursor-pointer",
+                rightPane === "js" && "border-b border-amber-400",
+              ].join(" ")}
+              onClick={() => setRightPane("js")}
+            >
+              JS Output
+            </button>
           </div>
         </div>
-        <Result value={compiledSource} />
-        {/* <Editor className="p-6" value={compiledSource} readOnly /> */}
+        {rightPane === "result" ? (
+          <Result className="p-6" value={compiledSource} />
+        ) : rightPane === "js" ? (
+          <Editor className="p-6" value={compiledSource} readOnly />
+        ) : null}
       </div>
     </SplitPane>
   );
