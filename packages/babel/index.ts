de92dc684e090da8b44e18175fc582c415604ae9
Lukas Bombach <lukas.bombach@googlemail.com>
2022-05-05 22:36:10 +0200

    that shit works, me likey

--- packages/babel/index.ts
+++ packages/babel/index.ts
@@ -1,10 +1,11 @@
+import template from "@babel/template";
 import type { NodePath, Node, Visitor } from "@babel/core";
 import type { JSXElement, JSXAttribute, ArrowFunctionExpression, FunctionExpression, Identifier } from "@babel/types";
 import type { Binding } from "@babel/traverse";
 
 interface State {
-  reactiveBindings: Binding[];
-  identifiers: NodePath<Identifier>[];
+  bindings: Binding[];
+  // identifiers: NodePath<Identifier>[];
 }
 
 function reactiveJsxPlugin(): { name: string; visitor: Visitor<State> } {
@@ -13,30 +14,58 @@ function reactiveJsxPlugin(): { name: string; visitor: Visitor<State> } {
     visitor: {
       Program: {
         enter(path, state) {
-          state.reactiveBindings = [];
-          state.identifiers = [];
+          state.bindings = [];
+          // state.identifiers = [];
 
           path.traverse({
-            JSXElement: path => {
-              collectIdentifiers(path, state);
-            },
+            // JSXElement: path => {
+            //   collectIdentifiers(path, state);
+            // },
             JSXAttribute: path => {
-              collectReactiveBindings(path, state);
+              collectbindings(path, state);
             },
           });
 
-          console.debug("reactiveBindings", state.reactiveBindings);
-          console.debug(
-            "identifiers",
-            state.identifiers.map(path => path.parentPath?.toString())
-          );
+          console.debug("bindings", state.bindings);
+
+          // console.debug(
+          //   "identifiers",
+          //   state.identifiers.map(path => path.parentPath?.toString())
+          // );
+
+          // path.traverse({
+          //   JSXAttribute: path => {
+          //     convertReferencesToGetters(path, state);
+          //   },
+          // });
+
+          const referencePaths = state.bindings
+            .flatMap(binding => binding.referencePaths)
+            .forEach(path => {
+              console.log(path);
+              path.replaceWith(getter({ GETTER: path.node.name }));
+            });
         },
       },
     },
   };
 }
 
-function collectReactiveBindings(path: NodePath<JSXAttribute>, state: State) {
+const getter = template.statement`
+  GETTER()
+`;
+
+// function convertReferencesToGetters(path: NodePath<JSXAttribute>, state: State) {
+//   path.traverse({
+//     Identifier: path => {
+//       const isAssignment = path.parentPath?.isAssignmentExpression() && path.parentPath?.get("left") === path;
+//       if (isAssignment) return;
+//       state.identifiers.push(path);
+//     },
+//   });
+// }
+
+function collectbindings(path: NodePath<JSXAttribute>, state: State) {
   if (!isEventHandler(path)) return;
 
   // todo this can be simplified by traversing the path directly (I guess?)
@@ -57,21 +86,21 @@ function collectReactiveBindings(path: NodePath<JSXAttribute>, state: State) {
         if (binding.kind === "const") return;
         if (binding.constantViolations.length === 0) return;
 
-        state.reactiveBindings.push(binding);
+        state.bindings.push(binding);
       },
     });
   }
 }
 
-function collectIdentifiers(path: NodePath<JSXElement>, state: State) {
-  path.traverse({
-    Identifier: path => {
-      const isAssignment = path.parentPath?.isAssignmentExpression() && path.parentPath?.get("left") === path;
-      if (isAssignment) return;
-      state.identifiers.push(path);
-    },
-  });
-}
+// function collectIdentifiers(path: NodePath<JSXElement>, state: State) {
+//   path.traverse({
+//     Identifier: path => {
+//       const isAssignment = path.parentPath?.isAssignmentExpression() && path.parentPath?.get("left") === path;
+//       if (isAssignment) return;
+//       state.identifiers.push(path);
+//     },
+//   });
+// }
 
 function isEventHandler(path: NodePath<JSXAttribute>): boolean {
   const identifier = path.get("name").node.name;
