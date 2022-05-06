90a8a29a6719af7d5da82fa94cc55d09cf8016fe
Lukas Bombach <lukas.bombach@googlemail.com>
2022-05-06 14:04:39 +0200

    whatever

--- packages/babel/index.ts
+++ packages/babel/index.ts
@@ -1,5 +1,5 @@
 import template from "@babel/template";
-import { AssignmentExpression, cloneDeepWithoutLoc } from "@babel/types";
+import { AssignmentExpression, cloneDeepWithoutLoc, Expression, isIdentifier } from "@babel/types";
 
 import type { NodePath, Node, Visitor } from "@babel/core";
 import type { JSXAttribute, ArrowFunctionExpression, FunctionExpression, Identifier } from "@babel/types";
@@ -29,40 +29,40 @@ function reactiveJsxPlugin(): { name: string; visitor: Visitor<State> } {
             .flatMap(binding => binding.referencePaths)
             .filter((path): path is NodePath<Identifier> => path.isIdentifier())
             .forEach(path => {
-              path.replaceWith(getter({ NAME: path.node.name }));
+              path.replaceWith(getter({ GETTER: path.node.name }));
             });
 
           state.bindings
             .flatMap(binding => binding.constantViolations)
             .filter((path): path is NodePath<AssignmentExpression> => path.isAssignmentExpression())
             .forEach(path => {
-              console.log(path);
-              // const NAME = path.node.left;
-              // const VALUE = cloneDeepWithoutLoc(path.node.right);
-              // path.replaceWith(setter({ NAME: path.node.name }));
+              if (!isIdentifier(path.node.left)) return;
+              const name = path.node.left.name;
+              const value = cloneDeepWithoutLoc(path.node.right);
+              path.replaceWith(createSetter(name, value));
             });
+
+          state.bindings.forEach(binding => {
+            const VALUE = cloneDeepWithoutLoc(binding.path.node.init);
+            const GETTER = binding.path.node.name;
+            const SETTER = `set${GETTER[0].toUpperCase()}${GETTER.substring(1)}`;
+
+            binding.path.parentPath.replaceWith(declaration({ GETTER, SETTER, VALUE }));
+          });
         },
       },
     },
   };
 }
 
-const getter = template.statement`
-  NAME()
-`;
-
-const setter = template.statement`
-  NAME(VALUE)
-`;
-
 function collectbindings(path: NodePath<JSXAttribute>, state: State) {
   if (!isEventHandler(path)) return;
 
   // todo this can be simplified by traversing the path directly (I guess?)
   const expression = path.get("value").get("expression");
-  assertNotAnArray(expression);
+  if (Array.isArray(expression)) return;
 
-  if (isAnyTypeOfFunctionExpression(expression)) {
+  if (isFunctionExpression(expression)) {
     expression.traverse({
       AssignmentExpression: path => {
         const left = path.get("left");
@@ -88,16 +88,25 @@ function isEventHandler(path: NodePath<JSXAttribute>): boolean {
   return /^on[A-Z]/.test(name);
 }
 
-function isAnyTypeOfFunctionExpression(
-  path: NodePath<Node>
-): path is NodePath<ArrowFunctionExpression | FunctionExpression> {
+function isFunctionExpression(path: NodePath<Node>): path is NodePath<ArrowFunctionExpression | FunctionExpression> {
   return path.isFunctionExpression() || path.isArrowFunctionExpression();
 }
 
-function assertNotAnArray<T>(value: T | T[]): asserts value is T {
-  if (Array.isArray(value)) {
-    throw new Error("Expected value not to be an array");
-  }
+function createSetter(name: string, VALUE: Expression) {
+  const SETTER = `set${name[0].toUpperCase()}${name.substring(1)}`;
+  return setter({ SETTER, VALUE });
 }
+
+const declaration = template.statement`
+  const [GETTER, SETTER] = ReactiveJsx.value(VALUE);
+`;
+
+const getter = template.statement`
+  GETTER()
+`;
+
+const setter = template.statement`
+  SETTER(VALUE)
+`;
 
 export default reactiveJsxPlugin;
