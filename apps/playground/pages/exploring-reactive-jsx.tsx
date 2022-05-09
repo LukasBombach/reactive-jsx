296d7c0c5e3f7a2e2397d57983191a960ba1160f
Lukas Bombach <lukas.bombach@googlemail.com>
2022-05-09 15:40:38 +0200

    renamed the pages

---
+++ apps/playground/pages/exploring-reactive-jsx.tsx
@@ -0,0 +1,85 @@
+import { Playground } from "@reactive-jsx/playground";
+
+const initialExample = `
+const Component = () => {
+  let count = 0;
+
+  return (
+    <button onClick={() => count = count + 1}>
+      Clicked {count}
+    </button>
+  )
+};
+
+document.body.append(<Component />);`;
+
+const insteadOfReactContext = `
+let count = 42;
+
+const Button = () => (
+  <button onClick={() => count = count + 1}>
+    Clicked {count}
+  </button>
+);
+
+const Result = () => <h1>The answer is {count}</h1>;
+
+document.body.append(<Button />, <Result />);`;
+
+const developmentCode1 = `
+const first = 'Rick';
+const last = 'Astley';
+
+const Name = ({ name }) => {
+  let age = 30;
+
+  return (
+    <button onClick={() => { age = age + 1; console.log(age); }} name={\`button-\${age}\`} value={age}>
+      His name is {name}. He is {age} years old.
+    </button>
+  );
+};
+
+document.body.append(<Name name={first} />);`;
+
+const developmentCode2 = `
+const name = "John";
+
+const Age = ({ name }) => {
+  let age = 30;
+
+  age = age + 2;
+
+  return (
+    <button onClick={() => age = age + 1} tabIndex="0" value={age} name={\`button with age of \${age}\`}>
+      {name}'s age is {age % 2 === 0 ? "even" : "odd"} ({age})
+    </button>
+  );
+};
+
+document.body.append(<Age name={name} />);`;
+
+export default function Home() {
+  return (
+    <main className="px-8 py-4 sm:px-16 sm:py-12 xl:px-48 xl:py-24 grid gap-8 text-slate-800 bg-grey-50">
+      <h1 className="leading-tight text-7xl max-w-screen-sm font-garamond">Exploring compile time reactive JSX</h1>
+      <Playground className="max-w-screen-lg" source={developmentCode2} resolveFile={() => null} />
+      {/* <blockquote className="pl-3 border-l-4 border-slate-200 text-slate-500 max-w-screen-sm">
+        Note: When this shows up in the git history, this text will be super emberrassing. I am writing this text only
+        to find a narrative which I can write code examples for. This way I can find an idea which features to
+        implement.
+      </blockquote>
+      <p className="max-w-screen-sm">What if you could write react components like this?</p>
+      <Playground className="max-w-screen-lg" source={initialExample} resolveFile={() => null} />
+      <p className="max-w-screen-sm">
+        No <code>setCount</code>, no <code>useEffect</code>. You just define a variable and then change it. The
+        component is updated accordingly.
+      </p>
+      <p className="max-w-screen-sm">
+        Well, you can, try it. You find <code>Context</code> annoying and a hassle to work with? So do I, why can't we
+        just define a variable and use it in more than one component? You can!
+      </p>
+      <Playground className="max-w-screen-lg" source={insteadOfReactContext} resolveFile={() => null} /> */}
+    </main>
+  );
+}
--- apps/playground/pages/index.tsx
+++ apps/playground/pages/index.tsx
@@ -1,85 +1,130 @@
-import { Playground } from "@reactive-jsx/playground";
+import type { FC, ReactNode } from "react";
 
-const initialExample = `
-const Component = () => {
-  let count = 0;
+type TC<P = {}> = FC<{ children: ReactNode } & P>;
 
-  return (
-    <button onClick={() => count = count + 1}>
-      Clicked {count}
-    </button>
-  )
-};
+const linkBrands = {
+  twitter: "text-[#55ACEE]",
+  github: "text-[#333333] dark:text-[#cdd9e5] font-medium",
+  dribbble: "text-[#EA4C89]",
+  tonline: "text-[#EA4C89]",
+} as const;
 
-document.body.append(<Component />);`;
-
-const insteadOfReactContext = `
-let count = 42;
-
-const Button = () => (
-  <button onClick={() => count = count + 1}>
-    Clicked {count}
-  </button>
+const DevTo = () => (
+  <picture>
+    <img
+      src="dev-black.png"
+      alt="Logo of the website dev.to"
+      width={1998 / 20}
+      height={1998 / 20}
+      className="inline-block h-[1em] w-auto border rounded dark:border-slate-400"
+    />
+  </picture>
 );
 
-const Result = () => <h1>The answer is {count}</h1>;
+const Medium = () => (
+  <picture>
+    <source srcSet="Medium-Logo-White-RGB@1x.png" media="(prefers-color-scheme: dark)"></source>
+    <img
+      src="Medium-Logo-Black-RGB@1x.png"
+      alt="Logo of the website Medium.com"
+      width={4488 / 44}
+      height={1114 / 44}
+      className="inline-block h-[1.2em] w-auto border rounded dark:border-slate-400"
+    />
+  </picture>
+);
 
-document.body.append(<Button />, <Result />);`;
+const Layout: TC = ({ children }) => (
+  <div className="grid grid-cols-1 gap-y-12 sm:gap-y-16 p-8 sm:p-24">{children}</div>
+);
 
-const developmentCode1 = `
-const first = 'Rick';
-const last = 'Astley';
+const H1: TC = ({ children }) => <h1 className="text-4xl font-garamond pb-5 sm:pb-8 max-w-screen-sm">{children}</h1>;
+const Intro: TC = ({ children }) => <p className="leading-7 text-sm max-w-screen-sm">{children}</p>;
+const H2: TC = ({ children }) => <h1 className="font-bold text-lg pb-1 sm:pb-2 max-w-screen-sm">{children}</h1>;
 
-const Name = ({ name }) => {
-  let age = 30;
+const BrandLink: TC<{
+  href: string;
+  brand?: keyof typeof linkBrands;
+}> = p => (
+  <a href={p.href} className={linkBrands[p.brand]}>
+    {p.children}
+  </a>
+);
 
+const ArticleList: TC = ({ children }) => (
+  <ul className="grid grid-cols-1 divide-y dark:divide-slate-700 text-sm max-w-screen-sm leading-6">{children}</ul>
+);
+
+const Article: TC<{ href: string }> = p => (
+  <li className="py-4">
+    <a href={p.href}>{p.children}</a>
+  </li>
+);
+
+const Title: TC = ({ children }) => <span className="block font-semibold">{children}</span>;
+const Description: TC = ({ children }) => <span className="block text-slate-500">{children}</span>;
+
+const Fin = () => <span className="italic text-slate-300 text-sm">Le Fin.</span>;
+
+export default function HomePage() {
   return (
-    <button onClick={() => { age = age + 1; console.log(age); }} name={\`button-\${age}\`} value={age}>
-      His name is {name}. He is {age} years old.
-    </button>
-  );
-};
-
-document.body.append(<Name name={first} />);`;
-
-const developmentCode2 = `
-const name = "John";
-
-const Age = ({ name }) => {
-  let age = 30;
-
-  age = age + 2;
-
-  return (
-    <button onClick={() => age = age + 1} tabIndex="0" value={age} name={\`button with age of \${age}\`}>
-      {name}'s age is {age % 2 === 0 ? "even" : "odd"} ({age})
-    </button>
-  );
-};
-
-document.body.append(<Age name={name} />);`;
-
-export default function Home() {
-  return (
-    <main className="px-8 py-4 sm:px-16 sm:py-12 xl:px-48 xl:py-24 grid gap-8 text-slate-800 bg-grey-50">
-      <h1 className="leading-tight text-7xl max-w-screen-sm font-garamond">Exploring compile time reactive JSX</h1>
-      <Playground className="max-w-screen-lg" source={developmentCode2} resolveFile={() => null} />
-      {/* <blockquote className="pl-3 border-l-4 border-slate-200 text-slate-500 max-w-screen-sm">
-        Note: When this shows up in the git history, this text will be super emberrassing. I am writing this text only
-        to find a narrative which I can write code examples for. This way I can find an idea which features to
-        implement.
-      </blockquote>
-      <p className="max-w-screen-sm">What if you could write react components like this?</p>
-      <Playground className="max-w-screen-lg" source={initialExample} resolveFile={() => null} />
-      <p className="max-w-screen-sm">
-        No <code>setCount</code>, no <code>useEffect</code>. You just define a variable and then change it. The
-        component is updated accordingly.
-      </p>
-      <p className="max-w-screen-sm">
-        Well, you can, try it. You find <code>Context</code> annoying and a hassle to work with? So do I, why can't we
-        just define a variable and use it in more than one component? You can!
-      </p>
-      <Playground className="max-w-screen-lg" source={insteadOfReactContext} resolveFile={() => null} /> */}
-    </main>
+    <Layout>
+      <header>
+        <H1>Unfinished thought</H1>
+        <Intro>
+          Hi, my name is Lukas Bombach, I work as a front end developer for{" "}
+          <BrandLink brand="tonline" href="https://www.t-online.de/">
+            t-online.de
+          </BrandLink>{" "}
+          in Berlin.
+          <br />
+          <span className="hidden sm:inline">
+            This is my blog where I post experimental code and thoughts on software development.
+            <br />
+          </span>
+          You can find me elsewhere on{" "}
+          <BrandLink brand="twitter" href="https://twitter.com/luke_schmuke">
+            Twitter
+          </BrandLink>
+          ,{" "}
+          <BrandLink brand="github" href="https://github.com/LukasBombach/">
+            GitHub
+          </BrandLink>{" "}
+          and{" "}
+          <BrandLink brand="dribbble" href="https://dribbble.com/luke_schmuke">
+            Dribbble
+          </BrandLink>
+          .
+        </Intro>
+      </header>
+      <main>
+        <H2>Articles</H2>
+        <ArticleList>
+          <Article href="/exploring-reactive-jsx">
+            <Title>Exploring compile time reactive JSX</Title>
+            <Description>Writing simpler components by transpiling away the hard parts</Description>
+          </Article>
+          <Article href="https://dev.to/lukasbombach/how-to-write-a-tree-shakable-component-library-4ied">
+            <Title>
+              How to write a tree-shakable component library <DevTo />
+            </Title>
+            <Description>
+              You don't need all components on all pages. A walkthrough on making tree-shaking happen
+            </Description>
+          </Article>
+          <Article href="https://medium.com/@luke_schmuke/how-we-achieved-the-best-web-performance-with-partial-hydration-20fab9c808d5">
+            <Title>
+              The case of partial hydration (with Next and Preact) <Medium />
+            </Title>
+            <Description>
+              Less JavaScript means better performance. Better performance means a better user experience
+            </Description>
+          </Article>
+        </ArticleList>
+      </main>
+      <footer>
+        <Fin />
+      </footer>
+    </Layout>
   );
 }
