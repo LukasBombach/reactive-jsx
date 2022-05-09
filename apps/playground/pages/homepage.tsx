2c04f2dc2ecb7cbb8fa6c10deab35a62201042c5
Lukas Bombach <lukas.bombach@googlemail.com>
2022-05-09 11:11:43 +0200

    things look kind of nice now

--- apps/playground/pages/homepage.tsx
+++ apps/playground/pages/homepage.tsx
@@ -9,34 +9,33 @@ const linkBrands = {
   tonline: "text-[#EA4C89]",
 } as const;
 
-const articleLogos = {
-  devto: (
-    <picture>
-      <img
-        src="dev-black.png"
-        alt="Logo of the website dev.to"
-        width={1998 / 20}
-        height={1998 / 20}
-        className="inline-block h-[1em] w-auto border rounded dark:border-slate-400"
-      />
-    </picture>
-  ),
-  medium: (
-    <picture>
-      <source srcSet="Medium-Logo-White-RGB@1x.png" media="(prefers-color-scheme: dark)"></source>
-      <img
-        src="Medium-Logo-Black-RGB@1x.png"
-        alt="Logo of the website Medium.com"
-        width={4488 / 44}
-        height={1114 / 44}
-        className="inline-block h-[1.2em] w-auto border rounded dark:border-slate-400"
-      />
-    </picture>
-  ),
-} as const;
+const DevTo = () => (
+  <picture>
+    <img
+      src="dev-black.png"
+      alt="Logo of the website dev.to"
+      width={1998 / 20}
+      height={1998 / 20}
+      className="inline-block h-[1em] w-auto border rounded dark:border-slate-400"
+    />
+  </picture>
+);
+
+const Medium = () => (
+  <picture>
+    <source srcSet="Medium-Logo-White-RGB@1x.png" media="(prefers-color-scheme: dark)"></source>
+    <img
+      src="Medium-Logo-Black-RGB@1x.png"
+      alt="Logo of the website Medium.com"
+      width={4488 / 44}
+      height={1114 / 44}
+      className="inline-block h-[1.2em] w-auto border rounded dark:border-slate-400"
+    />
+  </picture>
+);
 
 const Layout: TC = ({ children }) => (
-  <div className="grid grid-cols-1 gap-y-10 px-8 py-4 sm:px-16 sm:py-12 sm:gap-y-16 xl:px-24 xl:py-24">{children}</div>
+  <div className="grid grid-cols-1 gap-y-10 sm:gap-y-16 p-8 sm:p-24">{children}</div>
 );
 
 const H1: TC = ({ children }) => <h1 className="text-4xl font-garamond pb-4 sm:pb-8 max-w-screen-sm">{children}</h1>;
@@ -53,18 +52,19 @@ const BrandLink: TC<{
 );
 
 const ArticleList: TC = ({ children }) => (
-  <ul className="grid grid-cols-1 divide-y dark:divide-slate-700 text-sm max-w-screen-sm">{children}</ul>
+  <ul className="grid grid-cols-1 divide-y dark:divide-slate-700 text-sm max-w-screen-sm leading-6">{children}</ul>
 );
 
-const Article: TC<{ href: string; logo?: "medium" | "devto" }> = p => (
+const Article: TC<{ href: string }> = p => (
   <li className="py-4">
-    <a href={p.href}>
-      {p.children} {articleLogos[p.logo]}
-    </a>
+    <a href={p.href}>{p.children}</a>
   </li>
 );
 
-const Fin = () => <span className="italic text-slate-300 text-sm">fin</span>;
+const Title: TC = ({ children }) => <span className="block font-semibold">{children}</span>;
+const Description: TC = ({ children }) => <span className="block text-slate-500">{children}</span>;
+
+const Fin = () => <span className="italic text-slate-300 text-sm">Le Fin.</span>;
 
 export default function HomePage() {
   return (
@@ -100,15 +100,25 @@ export default function HomePage() {
       <main>
         <H2>Articles</H2>
         <ArticleList>
-          <Article href="/">Exploring compile time reactive JSX</Article>
-          <Article href="https://dev.to/lukasbombach/how-to-write-a-tree-shakable-component-library-4ied" logo="devto">
-            How to write a tree-shakable component library
+          <Article href="/">
+            <Title>Exploring compile time reactive JSX</Title>
+            <Description>Writing simpler components by transpiling away the hard parts</Description>
           </Article>
-          <Article
-            href="https://medium.com/@luke_schmuke/how-we-achieved-the-best-web-performance-with-partial-hydration-20fab9c808d5"
-            logo="medium"
-          >
-            The case of partial hydration (with Next and Preact)
+          <Article href="https://dev.to/lukasbombach/how-to-write-a-tree-shakable-component-library-4ied">
+            <Title>
+              How to write a tree-shakable component library <DevTo />
+            </Title>
+            <Description>
+              You probably don't need all components on all pages. A walkthrough on making tree-shaking happen
+            </Description>
+          </Article>
+          <Article href="https://medium.com/@luke_schmuke/how-we-achieved-the-best-web-performance-with-partial-hydration-20fab9c808d5">
+            <Title>
+              The case of partial hydration (with Next and Preact) <Medium />
+            </Title>
+            <Description>
+              Less JavaScript means better performance. Better performance means a better user experience
+            </Description>
           </Article>
         </ArticleList>
       </main>
