6561d7f1f90aadaeaf67aa9e009a41ffb395e559
Lukas Bombach <lukas.bombach@googlemail.com>
2022-05-09 09:44:42 +0200

    design is ok

--- apps/playground/pages/homepage.tsx
+++ apps/playground/pages/homepage.tsx
@@ -35,11 +35,13 @@ const articleLogos = {
   ),
 } as const;
 
-const Main: TC = ({ children }) => <main className="px-8 py-4 sm:px-16 sm:py-12 xl:px-24 xl:py-24">{children}</main>;
+const Layout: TC = ({ children }) => (
+  <div className="grid grid-cols-1 gap-y-10 px-8 py-4 sm:px-16 sm:py-12 sm:gap-y-16 xl:px-24 xl:py-24">{children}</div>
+);
 
-const H1: TC = ({ children }) => <h1 className="text-4xl font-garamond pb-8 max-w-screen-sm">{children}</h1>;
-const Intro: TC = ({ children }) => <p className="leading-7 text-sm pb-16 max-w-screen-sm">{children}</p>;
-const H2: TC = ({ children }) => <h1 className="font-bold text-lg pb-4 max-w-screen-sm">{children}</h1>;
+const H1: TC = ({ children }) => <h1 className="text-4xl font-garamond pb-4 sm:pb-8 max-w-screen-sm">{children}</h1>;
+const Intro: TC = ({ children }) => <p className="leading-7 text-sm max-w-screen-sm">{children}</p>;
+const H2: TC = ({ children }) => <h1 className="font-bold text-lg pb-2 sm:pb-4 max-w-screen-sm">{children}</h1>;
 
 const BrandLink: TC<{
   href: string;
@@ -51,7 +53,7 @@ const BrandLink: TC<{
 );
 
 const ArticleList: TC = ({ children }) => (
-  <ul className="grid grid-cols-1 divide-y dark:divide-slate-700 text-sm pb-16 max-w-screen-sm">{children}</ul>
+  <ul className="grid grid-cols-1 divide-y dark:divide-slate-700 text-sm max-w-screen-sm">{children}</ul>
 );
 
 const Article: TC<{ href: string; logo?: "medium" | "devto" }> = p => (
@@ -62,49 +64,57 @@ const Article: TC<{ href: string; logo?: "medium" | "devto" }> = p => (
   </li>
 );
 
-const Fin = () => <div className="italic text-slate-300 text-sm">fin</div>;
+const Fin = () => <span className="italic text-slate-300 text-sm">fin</span>;
 
 export default function HomePage() {
   return (
-    <Main>
-      <H1>Unfinished thought</H1>
-      <Intro>
-        Hi, my name is Lukas Bombach, I work as a front end developer for{" "}
-        <BrandLink brand="tonline" href="https://www.t-online.de/">
-          t-online.de
-        </BrandLink>{" "}
-        in Berlin.
-        <br />
-        This is my blog where I post experimental code and thoughts on software development.
-        <br />
-        You can find me elsewhere on{" "}
-        <BrandLink brand="twitter" href="https://twitter.com/luke_schmuke">
-          Twitter
-        </BrandLink>
-        ,{" "}
-        <BrandLink brand="github" href="https://github.com/LukasBombach/">
-          GitHub
-        </BrandLink>{" "}
-        and{" "}
-        <BrandLink brand="dribbble" href="https://dribbble.com/luke_schmuke">
-          Dribbble
-        </BrandLink>
-        .
-      </Intro>
-      <H2>Articles</H2>
-      <ArticleList>
-        <Article href="/">Exploring compile time reactive JSX</Article>
-        <Article href="https://dev.to/lukasbombach/how-to-write-a-tree-shakable-component-library-4ied" logo="devto">
-          How to write a tree-shakable component library
-        </Article>
-        <Article
-          href="https://medium.com/@luke_schmuke/how-we-achieved-the-best-web-performance-with-partial-hydration-20fab9c808d5"
-          logo="medium"
-        >
-          The case of partial hydration (with Next and Preact)
-        </Article>
-      </ArticleList>
-      <Fin />
-    </Main>
+    <Layout>
+      <header>
+        <H1>Unfinished thought</H1>
+        <Intro>
+          Hi, my name is Lukas Bombach, I work as a front end developer for{" "}
+          <BrandLink brand="tonline" href="https://www.t-online.de/">
+            t-online.de
+          </BrandLink>{" "}
+          in Berlin.
+          <br />
+          <span className="hidden sm:inline">
+            This is my blog where I post experimental code and thoughts on software development.
+            <br />
+          </span>
+          You can find me elsewhere on{" "}
+          <BrandLink brand="twitter" href="https://twitter.com/luke_schmuke">
+            Twitter
+          </BrandLink>
+          ,{" "}
+          <BrandLink brand="github" href="https://github.com/LukasBombach/">
+            GitHub
+          </BrandLink>{" "}
+          and{" "}
+          <BrandLink brand="dribbble" href="https://dribbble.com/luke_schmuke">
+            Dribbble
+          </BrandLink>
+          .
+        </Intro>
+      </header>
+      <main>
+        <H2>Articles</H2>
+        <ArticleList>
+          <Article href="/">Exploring compile time reactive JSX</Article>
+          <Article href="https://dev.to/lukasbombach/how-to-write-a-tree-shakable-component-library-4ied" logo="devto">
+            How to write a tree-shakable component library
+          </Article>
+          <Article
+            href="https://medium.com/@luke_schmuke/how-we-achieved-the-best-web-performance-with-partial-hydration-20fab9c808d5"
+            logo="medium"
+          >
+            The case of partial hydration (with Next and Preact)
+          </Article>
+        </ArticleList>
+      </main>
+      <footer>
+        <Fin />
+      </footer>
+    </Layout>
   );
 }
