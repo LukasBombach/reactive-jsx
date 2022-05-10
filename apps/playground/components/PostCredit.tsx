7e0efbaf1a5bd9bd0ca185b9b67f313e8a69ac1c
Lukas Bombach <lukas.bombach@googlemail.com>
2022-05-10 09:06:08 +0200

    tap styles

--- apps/playground/components/PostCredit.tsx
+++ apps/playground/components/PostCredit.tsx
@@ -7,7 +7,10 @@ export interface PostCreditProps {
 export const PostCredit: VFC<PostCreditProps> = ({ date }) => {
   const dateFormat = { weekday: "long", year: "numeric", month: "long", day: "numeric" } as const;
   return (
-    <a href="/" className="grid gap-x-2 items-center grid-cols-[40px_1fr] grid-rows-[40px] text-sm max-w-screen-sm">
+    <a
+      href="/"
+      className="grid gap-x-2 items-center grid-cols-[40px_1fr] grid-rows-[40px] text-sm max-w-screen-sm active:scale-[0.99] sm:active:scale-100"
+    >
       <img src="lukasbombach.jpg" alt="Portrait of the author Lukas Bombach" className="rounded-full w-full h-full" />
       <p className="">
         Lukas Bombach
