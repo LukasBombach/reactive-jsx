eb5dadc744f9ff06656ae899324c048b41d0d550
Lukas Bombach <lukas.bombach@googlemail.com>
2022-07-02 09:56:54 +0200

    wip

--- packages/runtime/src/render.ts
+++ packages/runtime/src/render.ts
@@ -1,33 +1,6 @@
-export type Tag = keyof JSX.IntrinsicElements;
+import type { Tag, Component, Props, Child, Element } from "./createElement";
 
-export type Component<P = {}> = (props?: P) => Element /*  | null */;
-
-export type ElementType = Tag | Component<any>;
-
-export type Props<T extends ElementType = ElementType> =
-  | (T extends Tag ? JSX.IntrinsicElements[T] : T extends Component<any> ? Omit<Parameters<T>[0], "children"> : never)
-  | null
-  | undefined;
-
-export type Attrs<P extends Props> = Omit<NonNullable<P>, "children">;
-
-export type Child = Element | string | number | boolean | null | undefined;
-
-export interface Element<T extends ElementType = ElementType> {
-  type: T;
-  props: Omit<Props<T> & { children: Child[] }, "key">;
-  key: string | number | null;
-}
-
-// todo typecast
-export function createElement<T extends Tag | Component<any>>(
-  type: T,
-  props?: Props<T>,
-  ...children: Child[]
-): Element<T> {
-  const { key = null, ...propsWithChildren } = { ...props, children };
-  return { type, props: propsWithChildren as Omit<Props<T> & { children: Child[] }, "key">, key };
-}
+type Attrs<P extends Props> = Omit<NonNullable<P>, "children">;
 
 export function render({ type, props }: Element): HTMLElement {
   const { children = [], ...attrs } = props || {};
