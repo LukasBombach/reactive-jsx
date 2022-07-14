2ae8245af5a62c4c62f4e5baae88c6c409dbe068
Lukas Bombach <lukas.bombach@googlemail.com>
2022-07-14 23:22:10 +0200

    finished getEventHandlers

--- packages/babel/src/plugin.mid.ts
+++ packages/babel/src/plugin.mid.ts
@@ -23,44 +23,7 @@ import type {
   FunctionDeclaration,
 } from "@babel/types";
 
-type SomeKindOfFunction = ArrowFunctionExpression | FunctionExpression | FunctionDeclaration;
-
-function getEventHandlers(path: NodePath<Node>): NodePath<SomeKindOfFunction>[] {
-  const functions: NodePath<SomeKindOfFunction>[] = [];
-
-  path.traverse({
-    JSXAttribute: path => {
-      const identifier = path.get("name").node.name;
-      const name = typeof identifier === "string" ? identifier : identifier.name;
-      if (!/^on[A-Z]/.test(name)) return;
-      const value = path.get("value");
-      if (!value.isJSXExpressionContainer()) return;
-      const expression = value.get("expression");
-      if (expression.isArrowFunctionExpression()) {
-        functions.push(expression);
-      }
-      if (expression.isFunctionExpression()) {
-        functions.push(expression);
-      }
-      if (expression.isIdentifier()) {
-        const binding = path.scope.getBinding(expression.node.name);
-        if (!binding) return;
-        if (binding.path.isFunctionDeclaration()) {
-          functions.push(binding.path);
-        }
-        if (binding.path.isVariableDeclarator()) {
-          const init = binding.path.get("init");
-          if (!init) return;
-          if (init.isArrowFunctionExpression() || init.isFunctionExpression()) {
-            functions.push(init);
-          }
-        }
-      }
-    },
-  });
-
-  return functions;
-}
+let runs = 0;
 
 function reactiveJsxPlugin(): { name: string; visitor: Visitor } {
   return {
@@ -403,6 +366,12 @@ function assignmentExpression(path: NodePath<Node>): path is NodePath<Assignment
 
 function updateExpression(path: NodePath<Node>): path is NodePath<UpdateExpression> {
   return path.isUpdateExpression();
+}
+
+function isEventHandler(path: NodePath<JSXAttribute>): boolean {
+  const identifier = path.get("name").node.name;
+  const name = typeof identifier === "string" ? identifier : identifier.name;
+  return /^on[A-Z]/.test(name);
 }
 
 export default reactiveJsxPlugin;
