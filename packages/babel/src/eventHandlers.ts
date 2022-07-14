36214fbb6ebbce1a76ad609113d2bdda43d8e9c2
Lukas Bombach <lukas.bombach@googlemail.com>
2022-07-14 23:55:17 +0200

    reboot cleanup

---
+++ packages/babel/src/eventHandlers.ts
@@ -0,0 +1,46 @@
+import type { NodePath, Node } from "@babel/core";
+import type { ArrowFunctionExpression, FunctionExpression, FunctionDeclaration } from "@babel/types";
+
+export type SomeKindOfFunction = ArrowFunctionExpression | FunctionExpression | FunctionDeclaration;
+
+export function getEventHandlers(path: NodePath<Node>): NodePath<SomeKindOfFunction>[] {
+  const functions: NodePath<SomeKindOfFunction>[] = [];
+
+  path.traverse({
+    JSXAttribute: path => {
+      const identifier = path.get("name").node.name;
+      const name = typeof identifier === "string" ? identifier : identifier.name;
+      if (!/^on[A-Z]/.test(name)) return;
+      const value = path.get("value");
+      if (!value.isJSXExpressionContainer()) return;
+      const expression = value.get("expression");
+
+      if (expression.isArrowFunctionExpression()) {
+        functions.push(expression);
+      }
+
+      if (expression.isFunctionExpression()) {
+        functions.push(expression);
+      }
+
+      if (expression.isIdentifier()) {
+        const binding = path.scope.getBinding(expression.node.name);
+        if (!binding) return;
+
+        if (binding.path.isFunctionDeclaration()) {
+          functions.push(binding.path);
+        }
+
+        if (binding.path.isVariableDeclarator()) {
+          const init = binding.path.get("init");
+          if (!init) return;
+          if (init.isArrowFunctionExpression() || init.isFunctionExpression()) {
+            functions.push(init);
+          }
+        }
+      }
+    },
+  });
+
+  return functions;
+}
--- packages/babel/src/plugin.ts
+++ packages/babel/src/plugin.ts
@@ -1,408 +1,43 @@
-import template from "@babel/template";
-import {
-  AssignmentExpression,
-  cloneDeepWithoutLoc,
-  Expression,
-  isIdentifier,
-  JSXExpressionContainer,
-} from "@babel/types";
+import { getEventHandlers } from "./eventHandlers";
 
-import type { NodePath, Node, Visitor, PluginPass } from "@babel/core";
-import type { Binding } from "@babel/traverse";
-import type {
-  ArrowFunctionExpression,
-  FunctionExpression,
-  Identifier,
-  JSXAttribute,
-  JSXElement,
-  Statement,
-  UpdateExpression,
-  VariableDeclarator,
-  Program,
-  VariableDeclaration,
-  FunctionDeclaration,
-} from "@babel/types";
+import type { NodePath, Node, Visitor } from "@babel/core";
+import type { Identifier, ArrowFunctionExpression, FunctionExpression, FunctionDeclaration } from "@babel/types";
 
-type SomeKindOfFunction = ArrowFunctionExpression | FunctionExpression | FunctionDeclaration;
-
-function getEventHandlers(path: NodePath<Node>): NodePath<SomeKindOfFunction>[] {
-  const functions: NodePath<SomeKindOfFunction>[] = [];
-
-  path.traverse({
-    JSXAttribute: path => {
-      const identifier = path.get("name").node.name;
-      const name = typeof identifier === "string" ? identifier : identifier.name;
-      if (!/^on[A-Z]/.test(name)) return;
-      const value = path.get("value");
-      if (!value.isJSXExpressionContainer()) return;
-      const expression = value.get("expression");
-      if (expression.isArrowFunctionExpression()) {
-        functions.push(expression);
-      }
-      if (expression.isFunctionExpression()) {
-        functions.push(expression);
-      }
-      if (expression.isIdentifier()) {
-        const binding = path.scope.getBinding(expression.node.name);
-        if (!binding) return;
-        if (binding.path.isFunctionDeclaration()) {
-          functions.push(binding.path);
-        }
-        if (binding.path.isVariableDeclarator()) {
-          const init = binding.path.get("init");
-          if (!init) return;
-          if (init.isArrowFunctionExpression() || init.isFunctionExpression()) {
-            functions.push(init);
-          }
-        }
-      }
-    },
-  });
-
-  return functions;
-}
-
-function reactiveJsxPlugin(): { name: string; visitor: Visitor } {
+export default function reactiveJsxPlugin(): { name: string; visitor: Visitor } {
   return {
     name: "Reactive JSX",
     visitor: {
       Program: {
         enter(path) {
-          // console.log("this is run #", ++runs);
-
-          // todo getBindingIdentifier || getOwnBindingIdentifier
-
-          const directBindings = getMutatedIdentifiersInEventHandlers(path)
-            .map(getBinding)
-            .filter(unique)
-            .filter(defined);
-
-          const indirectBindings = getAssignmentsUsingReferences(directBindings)
-            .map(getBinding)
-            .filter(unique)
-            .filter(defined);
-
-          const bindings = [...directBindings, ...indirectBindings];
-
-          console.log(bindings.flatMap(b => [b.path.type, b.path.toString()]));
-
-          // Convert usages to getters (`count` becomes `count()`)
-          bindings.forEach(binding => {
-            if (!binding.path.isVariableDeclarator()) return;
-            if (!isIdentifier(binding.path.node.id)) return;
-
-            const NAME = binding.path.node.id.name;
-
-            binding.referencePaths.filter(identifier).forEach(path => {
-              path.replaceWith(getter({ NAME }));
-            });
-          });
-
-          // Convert mutations to setters (`count = X` becomes `setCount()`)
-          bindings.forEach(binding => {
-            if (!binding.path.isVariableDeclarator()) return;
-            if (!isIdentifier(binding.path.node.id)) return;
-
-            const NAME = binding.path.node.id.name;
-
-            // AssignmentExpressions (`count = X` becomes `setCount(X)`)
-            binding.constantViolations.filter(assignmentExpression).forEach(path => {
-              // todo the worst code possible here
-              // todo validate begins with onX
-              const isInEventHandler = !!path.findParent(p => p.isJSXAttribute());
-
-              if (isInEventHandler) {
-                path.replaceWith(nonReactiveSetter({ NAME, VALUE: cloneDeepWithoutLoc(path.node.right) }));
-              } else {
-                path.replaceWith(setter({ NAME, VALUE: cloneDeepWithoutLoc(path.node.right) }));
-              }
-            });
-
-            // UpdateExpressions (`count++` becomes `setCount(count() + 1)`)
-            binding.constantViolations.filter(updateExpression).forEach(path => {
-              // todo the worst code possible here
-              // todo validate begins with onX
-              const isInEventHandler = !!path.findParent(p => p.isJSXAttribute());
-
-              const VALUE =
-                path.node.operator === "++"
-                  ? add({ NAME, VALUE: "1" })
-                  : path.node.operator === "--"
-                  ? sub({ NAME, VALUE: "1" })
-                  : null;
-
-              if (VALUE === null) {
-                throw new Error(`Unexpected operator ${path.node.operator}`);
-              }
-
-              if (isInEventHandler) {
-                path.replaceWith(nonReactiveSetter({ NAME, VALUE }));
-              } else {
-                path.replaceWith(setter({ NAME, VALUE }));
-              }
-            });
-          });
-
-          const statements: NodePath<Statement>[] = [];
-
-          bindings
-            .flatMap(binding => binding.referencePaths)
-            .filter(path => !(path.parentPath && cheapTempIsReactiveSetter(path.parentPath)))
-            .filter(path => !path.findParent(parent => parent.isJSXElement()))
-            .map(path => path.getStatementParent())
-            .filter((path): path is NodePath<Statement> => path !== null)
-            .filter(path => !path.isReturnStatement())
-            .filter(path => !statements.includes(path))
-            .filter(path => !statements.some(parent => path.isDescendant(parent)))
-            .forEach(path => statements.push(path));
-
-          function cheapTempIsReactiveSetter(path: NodePath<Node>): boolean {
-            return (
-              path.isCallExpression() &&
-              path.get("callee").isMemberExpression() &&
-              (path.get("callee").get("property") as NodePath<Identifier>)?.node?.name === "set"
-            );
-          }
-
-          statements.forEach(path => {
-            const VALUE = cloneDeepWithoutLoc(path.node);
-            path.replaceWith(reaction({ VALUE }));
-          });
-
-          // Convert declarations (`let count = X` becomes `const [count, setCount] = rjsx.val(X)`)
-
-          bindings.forEach(binding => {
-            if (!binding.path.isVariableDeclarator()) return;
-            if (!isIdentifier(binding.path.node.id)) return;
-
-            const NAME = binding.path.node.id.name;
-            const VALUE = binding.path.node.init ? cloneDeepWithoutLoc(binding.path.node.init) : "undefined";
-
-            binding.path.parentPath.replaceWith(declaration({ NAME, VALUE }));
-          });
-
-          // JSX Elements
-          path.traverse({
-            JSXElement: path => {
-              const attributes = path.get("openingElement").get("attributes");
-              const children = path.get("children");
-              const isComponent = openingElementIsCapitalized(path);
-
-              if (isComponent) return;
-
-              attributes
-                .filter((path): path is NodePath<JSXAttribute> => path.isJSXAttribute())
-                .filter(path => !isEventHandler(path))
-                .map(path => path.get("value"))
-                .filter((path): path is NodePath<JSXExpressionContainer> => path.isJSXExpressionContainer())
-                .map(path => path.get("expression"))
-                .filter((path): path is NodePath<Expression> => path.isExpression())
-                .forEach(path => {
-                  const VALUE = cloneDeepWithoutLoc(path.node);
-                  path.replaceWith(asFunction({ VALUE }));
-                });
-
-              children
-                .filter((path): path is NodePath<JSXExpressionContainer> => path.isJSXExpressionContainer())
-                .map(path => path.get("expression"))
-                .filter((path): path is NodePath<Expression> => path.isExpression())
-                .forEach(path => {
-                  const VALUE = cloneDeepWithoutLoc(path.node);
-                  path.replaceWith(asFunction({ VALUE }));
-                });
-            },
-          });
-
-          // Inject runtime
-          path.unshiftContainer("body", runtimeImports());
+          const eventHandlers = getEventHandlers(path);
+          const mutations = getMutatedVariables(eventHandlers);
         },
       },
     },
   };
 }
 
-function openingElementIsCapitalized(path: NodePath<JSXElement>): boolean {
-  const namePath = path.get("openingElement").get("name");
-  if (!namePath.isJSXIdentifier()) return false;
-  return /[A-Z]/.test(namePath.node.name.charAt(0));
+type SomeKindOfFunction = ArrowFunctionExpression | FunctionExpression | FunctionDeclaration;
+
+function getMutatedVariables(functions: NodePath<SomeKindOfFunction>[]): NodePath<Identifier>[] {
+  const identifiers: NodePath<Identifier>[] = [];
+
+  functions.forEach(path =>
+    path.traverse({
+      AssignmentExpression: path => {
+        const left = path.get("left");
+        if (left.isIdentifier()) identifiers.push(left);
+      },
+      UpdateExpression: path => {
+        const argument = path.get("argument");
+        if (argument.isIdentifier()) identifiers.push(argument);
+      },
+      CallExpression: path => {
+        const callee = path.get("callee");
+        // todo follow function call
+      },
+    })
+  );
+
+  return identifiers;
 }
-
-const asFunction = template.statement`
-  () => VALUE
-`;
-
-const runtimeImports = template.smart(`
-  import rjsx from "@reactive-jsx/runtime";
-`);
-
-const declaration = template.statement`
-  const NAME = rjsx.value(() => VALUE, "NAME");
-`;
-
-const getter = template.statement`
-  NAME.get()
-`;
-
-const setter = template.statement`
-  NAME.set(() => VALUE)
-`;
-
-const nonReactiveSetter = template.statement`
-  NAME.set(VALUE)
-`;
-
-const add = template.expression`
-  NAME.get() + VALUE
-`;
-
-const sub = template.expression`
-  NAME.get() - VALUE
-`;
-
-const reaction = template.statement`
-  rjsx.react(() => { VALUE });
-`;
-
-function followIdentifierIfFunction(path: NodePath<Node>, identifiers: NodePath<Node>[]) {
-  if (!path.isIdentifier()) return;
-  const binding = path.scope.getBinding(path.node.name);
-  if (!binding) return;
-  if (binding.path.isFunctionDeclaration()) {
-    followFunctionForMutations(binding.path, identifiers);
-  }
-  if (binding.path.isVariableDeclarator()) {
-    const init = binding.path.get("init");
-    if (!init) return;
-    if (init.isArrowFunctionExpression() || init.isFunctionExpression()) {
-      followFunctionForMutations(init, identifiers);
-    }
-  }
-}
-
-function followFunctionForMutations(
-  path: NodePath<FunctionDeclaration | ArrowFunctionExpression | FunctionExpression>,
-  identifiers: NodePath<Node>[]
-) {
-  path.traverse({
-    AssignmentExpression: path => {
-      identifiers.push(path.get("left"));
-    },
-    UpdateExpression: path => {
-      identifiers.push(path.get("argument"));
-    },
-    CallExpression: path => {
-      const callee = path.get("callee");
-      // todo
-      // debugger;
-      // console.log(callee);
-      followIdentifierIfFunction(callee, identifiers);
-    },
-  });
-}
-
-function getMutatedIdentifiersInEventHandlers(path: NodePath<Program>): NodePath<Identifier>[] {
-  const identifiers: NodePath<Node>[] = [];
-
-  path.traverse({
-    JSXAttribute: path => {
-      if (isEventHandler(path)) {
-        path.traverse({
-          AssignmentExpression: path => {
-            identifiers.push(path.get("left"));
-          },
-          UpdateExpression: path => {
-            identifiers.push(path.get("argument"));
-          },
-          Identifier: path => {
-            followIdentifierIfFunction(path, identifiers);
-          },
-        });
-      }
-    },
-  });
-
-  return identifiers.filter(identifier);
-}
-
-function getAssignmentsUsingReferences(bindings: Binding[]): NodePath<Identifier>[] {
-  return bindings
-    .flatMap(binding => binding.referencePaths)
-    .map(path => path.getStatementParent())
-    .filter(defined)
-    .flatMap(path => [...getIdentifiersFromVariableDeclaration(path), ...getIdentifiersFromExpressionStatement(path)]);
-}
-
-function isComponent(path: NodePath<VariableDeclarator>): boolean {
-  const init = path.get("init");
-  return init.isArrowFunctionExpression() && init.get("body").isJSXElement();
-}
-
-function getIdentifiersFromVariableDeclaration(path: NodePath<Node>): NodePath<Identifier>[] {
-  if (path.isVariableDeclaration()) {
-    return path
-      .get("declarations")
-      .filter(p => !isComponent(p))
-      .map(path => path.get("id"))
-      .filter(identifier);
-  }
-  return [];
-}
-
-function getIdentifiersFromExpressionStatement(path: NodePath<Node>): NodePath<Identifier>[] {
-  if (path.isExpressionStatement()) {
-    return [path.get("expression")]
-      .filter(assignmentExpression)
-      .map(path => path.get("left"))
-      .filter(identifier);
-  }
-  return [];
-}
-
-function i(path: NodePath<Program>): NodePath<Identifier>[] {
-  const paths: NodePath<Node>[] = [];
-
-  path.traverse({
-    JSXAttribute: path => {
-      if (isEventHandler(path)) {
-        path.traverse({
-          AssignmentExpression: path => {
-            paths.push(path.get("left"));
-          },
-          UpdateExpression: path => {
-            paths.push(path.get("argument"));
-          },
-        });
-      }
-    },
-  });
-
-  return paths.filter(identifier);
-}
-
-function getBinding(path: NodePath<Identifier>): Binding | undefined {
-  if (!path.isIdentifier()) return;
-  const name = path.node.name;
-  return path.scope.getBinding(name);
-}
-
-function defined<T>(value: T): value is NonNullable<T> {
-  return value !== null && value !== undefined;
-}
-
-function unique<T>(value: T, index: number, array: T[]): boolean {
-  return array.indexOf(value) === index;
-}
-
-function identifier(path: NodePath<Node>): path is NodePath<Identifier> {
-  return path.isIdentifier();
-}
-
-function assignmentExpression(path: NodePath<Node>): path is NodePath<AssignmentExpression> {
-  return path.isAssignmentExpression();
-}
-
-function updateExpression(path: NodePath<Node>): path is NodePath<UpdateExpression> {
-  return path.isUpdateExpression();
-}
-
-export default reactiveJsxPlugin;
