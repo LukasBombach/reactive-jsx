8bdb4e98aaf4975cee09dc4fc7fc80d4b583a449
Lukas Bombach <lukas.bombach@googlemail.com>
2022-07-24 14:55:44 +0200

    more working specs

--- packages/babel/src/__test__/declarations.spec.ts
+++ packages/babel/src/__test__/declarations.spec.ts
@@ -4,11 +4,48 @@ import "./jest.extend";
 
 describe("declarations", () => {
   test("x", async () => {
-    await visit("let count = 0; const Button = () => <button onClick={() => count++} />;", {
-      Program: path => {
-        const declarations = getDeclarations(path);
-        expect(declarations).toMatchSourceCode(["count = 0"]);
-      },
-    });
+    await visit(
+      `
+      let count = 0;
+      const Button = () => <button>{count}</button>;
+    `,
+      {
+        Program: path => {
+          const declarations = getDeclarations(path);
+          expect(declarations).toMatchSourceCode([]);
+        },
+      }
+    );
+  });
+
+  test("x", async () => {
+    await visit(
+      `
+      let count = 0;
+      const Button = () => <button onClick={() => count++} />;
+    `,
+      {
+        Program: path => {
+          const declarations = getDeclarations(path);
+          expect(declarations).toMatchSourceCode(["count = 0"]);
+        },
+      }
+    );
+  });
+
+  test("x", async () => {
+    await visit(
+      `
+      let count = 0;
+      let count2 = 0;
+      const Button = () => <button onMouseDown={() => count++} onMouseUp={() => count2++} />;
+    `,
+      {
+        Program: path => {
+          const declarations = getDeclarations(path);
+          expect(declarations).toMatchSourceCode(["count = 0", "count2 = 0"]);
+        },
+      }
+    );
   });
 });
