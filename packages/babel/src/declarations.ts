4fe9bda690d351955b4f19d228f9f23e6b9f136c
Lukas Bombach <lukas.bombach@googlemail.com>
2022-07-24 15:09:21 +0200

    implemtented recursive assignments

--- packages/babel/src/declarations.ts
+++ packages/babel/src/declarations.ts
@@ -6,6 +6,7 @@ import { unique, isNonNullable } from "./typeGuards";
 
 import type { NodePath } from "@babel/core";
 import type { Program } from "@babel/types";
+import type { Binding } from "@babel/traverse";
 
 export function getDeclarations(path: NodePath<Program>) {
   const eventHandlers = getEventHandlers(path);
@@ -16,11 +17,17 @@ export function getDeclarations(path: NodePath<Program>) {
     .filter(isNonNullable)
     .filter(unique);
 
-  const assignedVariables = mutatedVariables
-    .flatMap(getAssignments)
-    .map(getDeclaration)
-    .filter(isNonNullable)
-    .filter(unique);
+  const assignedVariables = findAssignedVariables(mutatedVariables);
 
   return [...mutatedVariables, ...assignedVariables].filter(unique);
+}
+
+function findAssignedVariables(bindings: Binding[]): Binding[] {
+  const assignments = bindings.flatMap(getAssignments).map(getDeclaration).filter(isNonNullable).filter(unique);
+
+  if (assignments.length) {
+    return [...assignments, ...findAssignedVariables(assignments)].filter(unique);
+  }
+
+  return assignments;
 }
