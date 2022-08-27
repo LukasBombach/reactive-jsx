de575fffce2c211520c905ae2d9ab3927f17cba7
Lukas Bombach <lukas.bombach@googlemail.com>
2022-08-27 13:11:28 +0200

    many more tests

--- packages/babel/src/__test__/x.spec.tsx
+++ packages/babel/src/__test__/x.spec.tsx
@@ -47,6 +47,21 @@ test("positive assignment", () => {
   `);
 });
 
+test("positive assignment with another number", () => {
+  const code = t(`
+      const count = 5;
+      const Button = () => <button onClick={() => count = count + 5} />
+    `);
+
+  expect(code).toMatchInlineSnapshot(`
+    "const count = rjsx.value(() => 5, \\"count\\");
+
+    const Button = () => rjsx.createElement(\\"button\\", {
+      onClick: () => count.set(() => count.get() + 5)
+    });"
+  `);
+});
+
 test("negative assignment", () => {
   const code = t(`
       const count = 1;
@@ -58,6 +73,21 @@ test("negative assignment", () => {
 
     const Button = () => rjsx.createElement(\\"button\\", {
       onClick: () => count.set(() => count.get() - 1)
+    });"
+  `);
+});
+
+test("negative assignment with another number", () => {
+  const code = t(`
+      const count = 5;
+      const Button = () => <button onClick={() => count = count - 5} />
+    `);
+
+  expect(code).toMatchInlineSnapshot(`
+    "const count = rjsx.value(() => 5, \\"count\\");
+
+    const Button = () => rjsx.createElement(\\"button\\", {
+      onClick: () => count.set(() => count.get() - 5)
     });"
   `);
 });
@@ -128,6 +158,73 @@ test("Event handler reference with update", () => {
 
     const Button = () => rjsx.createElement(\\"button\\", {
       onClick: handleClick
+    });"
+  `);
+});
+
+test("Transitive event handlers", () => {
+  const code = t(`
+      const count = 1;
+      function handleClick() { inc(); }
+      function inc() { count = count + 1; }
+      const Button = () => <button onClick={handleClick} />
+    `);
+
+  expect(code).toMatchInlineSnapshot(`
+    "const count = rjsx.value(() => 1, \\"count\\");
+
+    function handleClick() {
+      inc();
+    }
+
+    function inc() {
+      count.set(() => count.get() + 1);
+    }
+
+    const Button = () => rjsx.createElement(\\"button\\", {
+      onClick: handleClick
+    });"
+  `);
+});
+
+test("shared variables (react context)", () => {
+  const code = t(`
+      const count = 1;
+      const Inc = () => <button onClick={() => count = count + 1} />
+      const Dec = () => <button onClick={() => count = count - 1} />
+      const Show = () => <pre>Num is {count}</pre>
+    `);
+
+  expect(code).toMatchInlineSnapshot(`
+    "const count = rjsx.value(() => 1, \\"count\\");
+
+    const Inc = () => rjsx.createElement(\\"button\\", {
+      onClick: () => count.set(() => count.get() + 1)
+    });
+
+    const Dec = () => rjsx.createElement(\\"button\\", {
+      onClick: () => count.set(() => count.get() - 1)
+    });
+
+    const Show = () => rjsx.createElement(\\"pre\\", null, \\"Num is \\", count.get());"
+  `);
+});
+
+test("side effects (use effect)", () => {
+  const code = t(`
+      const count = 1;
+      document.title = \`The count is \${count}\`;
+      const Button = () => <button onClick={() => count = count + 1} />
+    `);
+
+  expect(code).toMatchInlineSnapshot(`
+    "const count = rjsx.value(() => 1, \\"count\\");
+    rjsx.react(() => {
+      document.title = \`The count is \${count.get()}\`;
+    });
+
+    const Button = () => rjsx.createElement(\\"button\\", {
+      onClick: () => count.set(() => count.get() + 1)
     });"
   `);
 });
