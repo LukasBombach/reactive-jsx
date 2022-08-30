c158eb21bf70eee88f76219e4776ad50bbfe3937
Lukas Bombach <lukas.bombach@googlemail.com>
2022-08-30 21:41:37 +0200

    wrote text

--- apps/playground/pages/exploring-reactive-jsx.tsx
+++ apps/playground/pages/exploring-reactive-jsx.tsx
@@ -13,12 +13,32 @@ export default function ExploringReactiveJsx() {
       </header>
       <main>
         <P>
-          Writing
-          <strong className="pb-px border-b-[3px] border-b-sky-300">React is a great libary.</strong>
-          <strong className="pb-px border-b-[3px] border-b-sky-300">React is a great libary.</strong> It promotes a
-          clean and simple model for writing user interfaces. While not every problem is solved (or in its scope) it
-          allows With components we can define reusable chunks of our UI and orchestrate everything to a whole that is
-          greater than the sum of its parts.
+          Implementing user interfaces is by no means a trivial task. React has made this remarkably simply, relatively
+          speaking. Before React we had numerous contesters and a list of paradigms that came with abbreviations that
+          felt more like spelling contest than a … .
+        </P>
+
+        <P>
+          Among the more noteworthy ones were Backbone.js, which promoted an MVC-style paradigm as well as AngularJS,
+          which went one step further and promised a one stop solution to all needs a front end would ever have. Both
+          frameworks, I would argue, can safely be considered legacy now.
+        </P>
+
+        <P>
+          React is here since 2013 and still <em>hot</em>. Why? I think did one thing very well. It promoted a mental
+          model that actually fits in your head. Don&apos;t get me wrong, things still get messy, most code does, but
+          even then, the mess can be managed and cleaned up. The basic idea allow that.
+        </P>
+
+        <P>
+          All this comes down to the mental model of <Code>components</Code>, <Code>props</Code>, <Code>children</Code>{" "}
+          and <Code>state</Code>. Components encapsulate complexity and create manageble mental units. Props and
+          children define dependencies and relationships between units and state enables reactive programming in an
+          almost declarative way.
+        </P>
+
+        <P>
+          The way React handles <Code>state</Code>, to me, is one of the greatest tricks. Especially in the early days
         </P>
       </main>
     </Layout>
